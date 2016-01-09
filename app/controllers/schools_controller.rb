44970348708a388037af7ea9aba8b1ced3260e32
Chris Collinsworth <cmcollin41@gmail.com>
2016-01-08 17:53:32 -0700

    fixed active record sort by time

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -29,14 +29,12 @@ class SchoolsController < ApplicationController
     @schools = School.all
     @school = School.friendly.find(params[:id])
 
-    if params[:sort] == "Yesterday"
-      @school_conversations = @school.conversations.where("created_at < ? AND created_at >= ?" ,Time.zone.now.beginning_of_day, Time.zone.now.beginning_of_day - 24.hours).order("likes_count DESC, created_at DESC")
-    elsif params[:sort] == "Past Week" 
-      @school_conversations = @school.conversations.where("created_at < ? AND created_at >= ?", Time.zone.now.beginning_of_day, Time.zone.now.beginning_of_day - 168.hours).order("likes_count DESC, created_at DESC")
+    if params[:sort] == "Past Week"
+      @school_conversations = @school.conversations.where("created_at < ? AND created_at >= ?", Time.zone.now, Time.zone.now - 168.hours).order("likes_count DESC, created_at DESC")
     elsif params[:sort] == "Past Month"
-      @school_conversations = @school.conversations.where("created_at < ? AND created_at >= ?", Time.zone.now.beginning_of_day, Time.zone.now.beginning_of_day - 720.hours).order("likes_count DESC, created_at DESC")
+      @school_conversations = @school.conversations.where("created_at < ? AND created_at >= ?", Time.zone.now, Time.zone.now - 720.hours).order("likes_count DESC, created_at DESC")
     else
-      @school_conversations = @school.conversations.where("created_at >= ?", Time.zone.now.beginning_of_day).order("likes_count DESC, created_at DESC")
+      @school_conversations = @school.conversations.where("created_at >= ?", Time.zone.now - 24.hours).order("likes_count DESC, created_at DESC")
     end
 
   end
